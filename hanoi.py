74d26ace2e54c5a208f4a7acd24e5b70c79d6995
Chris Sims <Chris@Chriss-MacBook-Pro-3.local>
2019-02-20 17:31:58 -0500

    third

--- hanoi.py
+++ hanoi.py
@@ -1,37 +1,49 @@
 from hanoi_viz import *
 
-TOWERS = ['tower A', 'tower B', 'tower C']
+TOWERS = ['A', 'B', 'C']
 SOURCE_TOWER = TOWERS[0]
 TARGET_TOWER = TOWERS[1]
 MID_TOWER = TOWERS[2]
 MAX_HEIGHT = 8
 MIN_HEIGHT = 0
 
-def move_tower(towers, target_tower, source_tower, mid_tower, cnt, disk_count):
-    if cnt == (2**disk_count) - 1:
+
+def move_tower(disk, towers, target, source, mid, disk_count):
+    if disk == disk_count:
+        move_disk(source, target, towers)
         return
-    if len(towers[target_tower][cnt]) < len(source_tower[0]):
-        move_disk(source_tower, target_tower, towers)
-        #print(towers[target_tower], towers[source_tower])
-        #return
-    else:
-        move_disk(source_tower, target_tower, towers)
-    move_tower(towers, target_tower, source_tower, mid_tower, cnt+1, disk_count)
+    disk += 1
+    # move disk_count - 1 disks to mid
+    # mid is now target and target is now mid
+    move_tower(disk, towers, mid, source, target, disk_count)
+    print('called move_tower on target =', mid, 'and mid = ', target )
+    print('move from', source, 'to', target)
+    input('')
+    move_disk(source, target, towers)
+
+    move_tower(disk, towers, target, mid, source, disk_count)
+
 
 def main():
-    cnt = 0
+    disk = 0
     disk_count = int(input('how many disks?'))
+    recursive_thresh = (2 * disk_count) - 1
     towers = initialize_towers(disk_count,
                                TOWERS[0],
                                TOWERS[1],
                                TOWERS[2])
-    move_tower(towers, TARGET_TOWER, SOURCE_TOWER, MID_TOWER, cnt, disk_count)
+    move_tower(disk,
+               towers,
+               TARGET_TOWER,
+               SOURCE_TOWER,
+               MID_TOWER,
+               disk_count)
 
 
         # print('tar:', TARGET_TOWER[-1], 'src:', SOURCE_TOWER)
     # else:
         # print('tar:', TARGET_TOWER[-1], 'src:', MID_TOWER)
 
-    #print_towers(towers)
+    #print('test123')
 
 main()
