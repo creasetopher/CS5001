1e03b3c0bcbd3eb38cd2ae14a799bf10096277a8
Chris Sims <chrissims@Chriss-MacBook-Pro-2.local>
2019-02-20 23:09:03 -0500

    fourth

--- hanoi.py
+++ hanoi.py
@@ -4,46 +4,41 @@ TOWERS = ['A', 'B', 'C']
 SOURCE_TOWER = TOWERS[0]
 TARGET_TOWER = TOWERS[1]
 MID_TOWER = TOWERS[2]
-MAX_HEIGHT = 8
-MIN_HEIGHT = 0
 
 
-def move_tower(disk, towers, target, source, mid, disk_count):
-    if disk == disk_count:
+def move_tower(disk_count, towers, target, source, mid):
+    # when disk_count is 1, we are at bottom disk.
+    # recursion unwinds when disk_count is 0
+    if disk_count > 0:
+        # moves disk_count - 1 disks from source
+        # this will get us to the bottom/last disk at source
+        print('top', disk_count)
+        move_tower(disk_count - 1, towers, mid, source, target)
+        # move a disk from source to target
         move_disk(source, target, towers)
-        return
-    disk += 1
-    # move disk_count - 1 disks to mid
-    # mid is now target and target is now mid
-    move_tower(disk, towers, mid, source, target, disk_count)
-    print('called move_tower on target =', mid, 'and mid = ', target )
-    print('move from', source, 'to', target)
-    input('')
-    move_disk(source, target, towers)
+        # move disk from mid to target
+        print('bottom', disk_count)
+        move_tower(disk_count - 1, towers, target, mid, source)
 
-    move_tower(disk, towers, target, mid, source, disk_count)
 
 
 def main():
-    disk = 0
-    disk_count = int(input('how many disks?'))
-    recursive_thresh = (2 * disk_count) - 1
+    while True:
+        disk_count = input('how many disks? ')
+        if disk_count.isdigit():
+            disk_count = int(disk_count)
+            if disk_count >= 1 and disk_count <= 8:
+                break
+
     towers = initialize_towers(disk_count,
                                TOWERS[0],
                                TOWERS[1],
                                TOWERS[2])
-    move_tower(disk,
+    move_tower(disk_count,
                towers,
                TARGET_TOWER,
                SOURCE_TOWER,
-               MID_TOWER,
-               disk_count)
-
-
-        # print('tar:', TARGET_TOWER[-1], 'src:', SOURCE_TOWER)
-    # else:
-        # print('tar:', TARGET_TOWER[-1], 'src:', MID_TOWER)
-
-    #print('test123')
+               MID_TOWER
+               )
 
 main()
